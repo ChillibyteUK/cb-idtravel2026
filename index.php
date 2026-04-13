e9180d9576953be2d299dc7a32d47578530159b4
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-13 11:59:37 +0100

    index grid

--- index.php
+++ index.php
@@ -69,6 +69,18 @@ get_header();
 					case 3:
 						$col_class = 'col-md-3 insight-type-grid__card-3';
 						break;
+					case 4:
+						$col_class = 'col-md-6 insight-type-grid__card-4';
+						break;
+					case 5:
+						$col_class = 'col-md-3 insight-type-grid__card-5';
+						break;
+					case 6:
+						$col_class = 'col-md-3 insight-type-grid__card-6';
+						break;
+					case 7:
+						$col_class = 'col-md-12 insight-type-grid__card-7';
+						break;
 					default:
 						$col_class = 'col-md-6';
 						break;

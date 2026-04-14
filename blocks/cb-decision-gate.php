94f412f30e5848de95052f8cd05cc2af6cba2474
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-14 09:10:03 +0100

    decision gate

--- blocks/cb-decision-gate.php
+++ blocks/cb-decision-gate.php
@@ -19,16 +19,16 @@ $link_right = get_field( 'right_link' );
 ?>
 <section class="cb-decision-gate <?= esc_attr( trim( $bg . ' ' . $fg . ' ' . $extra ) ); ?>" id="<?= esc_attr( $section_id ); ?>">
 	<div class="id-container px-4 px-md-5 py-5">
-		<div class="row">
-			<div class="col-md-6 cb-decision-gate__left">
+		<div class="cb-decision-gate__grid">
+			<div class="cb-decision-gate__left">
 				<a href="<?= esc_url( $link_left['url'] ); ?>" aria-label="<?= esc_attr( $link_left['title'] ); ?>">
 					<div class="cb-decision-gate__title"><?= esc_html( get_field( 'left_title' ) ); ?></div>
 					<div class="cb-decision-gate__content"><?= wp_kses_post( get_field( 'left_content' ) ); ?></div>
 				</a>
 				<a href="<?= esc_url( $link_left['url'] ); ?>" class="id-button"><?= esc_html( $link_left['title'] ); ?></a>
 			</div>
-			<div class="col-md-6 cb-decision-gate__right">
-				<a href="<?= esc_url( $link_left['url'] ); ?>" aria-label="<?= esc_attr( $link_left['title'] ); ?>">
+			<div class="cb-decision-gate__right">
+				<a href="<?= esc_url( $link_right['url'] ); ?>" aria-label="<?= esc_attr( $link_right['title'] ); ?>">
 					<div class="cb-decision-gate__title"><?= esc_html( get_field( 'right_title' ) ); ?></div>
 					<div class="cb-decision-gate__content"><?= wp_kses_post( get_field( 'right_content' ) ); ?></div>
 				</a>

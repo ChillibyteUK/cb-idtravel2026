a274490a409fe6dd81fcc1773dd71c3fc10dc4c2
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-14 11:16:22 +0100

    signpost header line colour

--- blocks/cb-signpost-header.php
+++ blocks/cb-signpost-header.php
@@ -12,11 +12,20 @@ $bg         = ! empty( $block['backgroundColor'] ) ? 'has-' . $block['background
 $fg         = ! empty( $block['textColor'] ) ? 'has-' . $block['textColor'] . '-color' : '';
 $section_id = $block['anchor'] ?? null;
 $extra      = $block['className'] ?? '';
+$line_class = 'dark-lines';
+
+if ( ! empty( $block['backgroundColor'] ) ) {
+	if ( preg_match( '/(\d+)(?!.*\d)/', $block['backgroundColor'], $matches ) ) {
+		$line_class = (int) $matches[1] >= 600 ? 'light-lines' : 'dark-lines';
+	} else {
+		$line_class = 'light-lines';
+	}
+}
 
 $block_title = get_field( 'title' ) ?: 'Signpost Subheader';
 
 ?>
-<section class="cb-signpost-header <?= esc_attr( trim( $bg . ' ' . $fg . ' ' . $extra ) ); ?>" id="<?= esc_attr( $section_id ); ?>">
+<section class="cb-signpost-header <?= esc_attr( trim( $bg . ' ' . $fg . ' ' . $line_class . ' ' . $extra ) ); ?>" id="<?= esc_attr( $section_id ); ?>">
 	<div class="id-container px-4 px-md-5">
 		<?= esc_html( $block_title ); ?>
 	</div>

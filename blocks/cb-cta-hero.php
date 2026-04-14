e55fbad98e7683dffe1386647bb7c123966fe044
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-14 12:43:34 +0100

    cta hero

--- blocks/cb-cta-hero.php
+++ blocks/cb-cta-hero.php
@@ -10,6 +10,7 @@ defined( 'ABSPATH' ) || exit;
 $block_title = get_field( 'title' ) ? get_field( 'title' ) : get_the_title();
 
 $block_link = get_field( 'cta_link' );
+$has_block_link = ! empty( $block_link['url'] ) && ! empty( $block_link['title'] );
 
 ?>
 <section class="cta-hero">
@@ -20,15 +21,22 @@ $block_link = get_field( 'cta_link' );
 	</div>
 	<div class="id-container pt-2 pb-1 px-4 px-md-5">
 		<div class="row g-4">
+			<?php if ( $has_block_link ) : ?>
+				<div class="col-lg-8 cta-hero__content">
+					<?= wp_kses_post( get_field( 'content' ) ); ?>
+				</div>
+			<?php endif; ?>
 			<div class="col-lg-4">
-				<?php if ( ! empty( $block_link['url'] ) && ! empty( $block_link['title'] ) ) : ?>
+				<?php if ( $has_block_link ) : ?>
 					<a class="id-button mb-5" href="<?= esc_url( $block_link['url'] ); ?>" target="<?= esc_attr( $block_link['target'] ?: '_self' ); ?>"><?= esc_html( $block_link['title'] ); ?></a>
 				<?php endif; ?>
 				<div class="cta-hero__cta-title"><?= esc_html( get_field( 'cta_title' ) ); ?></div>
 			</div>
-			<div class="col-lg-8 cta-hero__content">
-				<?= wp_kses_post( get_field( 'content' ) ); ?>
-			</div>
+			<?php if ( ! $has_block_link ) : ?>
+				<div class="col-lg-8 cta-hero__content">
+					<?= wp_kses_post( get_field( 'content' ) ); ?>
+				</div>
+			<?php endif; ?>
 		</div>
 	</div>
 </section>
